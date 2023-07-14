7bd77fbd7b51271724445cdf0168905fb6cfef5f
Ian Shehadeh <ian@shehadeh.net>
2023-07-14 15:34:26 -0400

    clear bindings from doc, add location info

--- common/bindings.test.ts
+++ common/bindings.test.ts
@@ -1,8 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
-import { findBindings } from "./bindings.ts";
+import { findAndClearBindings } from "./bindings.ts";
 
 Deno.test("Path functions", () => {
-  const [binding] = findBindings("hello $[a:1.b ./*]");
+  const [newDoc, [binding]] = findAndClearBindings("hello $[a:1.b ./*]");
+  assertEquals(newDoc, "hello ");
   assertEquals(binding.objectSourceName, "a");
   assertEquals(binding.objectID, "1");
   assertEquals(binding.objectPropertyPath, ".b");
--- common/bindings.ts
+++ common/bindings.ts
@@ -1,4 +1,5 @@
 import { globToRegExp } from "https://deno.land/std@0.189.0/path/glob.ts";
+import { SyntaxNode, SyntaxNodeRef } from "./deps.ts";
 
 // testing out binding syntax:
 //
@@ -8,19 +9,41 @@ import { globToRegExp } from "https://deno.land/std@0.189.0/path/glob.ts";
 // This means “bind the name property of the object with ID {{id}} from the ‘task’ data source to the value of the current syntax node,
 // and decendants (`**`), not including ‘DueDate’ syntax nodes.”
 
+/// 2 ways to identify a binding's location:
+///
+/// By Index
+/// ------------
+/// Not ideal, should be used as briefly as possible to get the syntax node ref.
+/// Used When:
+///  - initially, before the document has been parsed.
+///  - The syntax node has been deleted (index is set to the syntax node's last index), [TODO: would this work right?]
+///
+/// By Node
+/// ------------
+/// General way to match a binding. `node` and all of its decendants with paths matching pattern `syntaxPattern` are the binding's value.
+type BindingLocation = { index: number } | { node: SyntaxNodeRef };
+
 type Binding = {
+  location: BindingLocation;
+
   objectSourceName: string;
   objectID: string; // integer IDs only right now, using a string for flexibility + precision
   objectPropertyPath: string;
   syntaxPattern: RegExp;
 };
 
-export function* findBindings(doc: string): Generator<Binding> {
+/// Awful, awful mini-templating language implementated with regex, basically.
+export function findAndClearBindings(
+  doc: string,
+): [string, Binding[]] {
   // should probably make a grammar for these, using regex for prototype
   const BINDING_RE = /(?<!\\)\$\[([^\]]+)\]/gu;
   const BINDING_PATH_RE = /([a-z_][a-z0-9_]*):(\d+)(\.[a-z_][a-z0-9_]*)+/iu;
 
-  for (const binding of doc.matchAll(BINDING_RE)) {
+  const bindings = [];
+  let newDoc = doc;
+  let binding = null;
+  while ((binding = BINDING_RE.exec(newDoc)) !== null) {
     try {
       const [objectPath, syntaxGlob] = binding[1].split(" ");
       const objectPathMatch = objectPath.match(BINDING_PATH_RE)!;
@@ -30,15 +53,30 @@ export function* findBindings(doc: string): Generator<Binding> {
         os: "linux",
         globstar: true,
       });
-
-      yield {
+      // index is the index of the binding *after* preprocessing
+      // since all prior bindings have been removed
+      bindings.push({
+        location: { index: binding.index! },
         objectSourceName: objectPathMatch[1],
         objectID: objectPathMatch[2],
         objectPropertyPath: objectPathMatch[3],
         syntaxPattern,
-      };
+      });
     } catch (e) {
       throw new Error(`failed to parse binding '${binding}': ${e}`);
     }
+
+    newDoc = newDoc.slice(0, binding.index!) +
+      newDoc.slice(binding.index! + binding[0].length);
+  }
+
+  return [newDoc, bindings];
+}
+
+export class BindingStore {
+  bindings: Binding[];
+
+  constructor(bindings: Binding[]) {
+    this.bindings = bindings;
   }
 }
