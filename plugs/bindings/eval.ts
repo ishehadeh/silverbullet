a756499557dea78c01bf5a54f7aab5d1a807e113
Ian Shehadeh <ian@shehadeh.net>
2023-07-29 18:55:16 -0400

    basic add in binding expr

--- plugs/bindings/eval.ts
+++ plugs/bindings/eval.ts
@@ -12,7 +12,27 @@ export function evalExpression(
     case "Number":
       return Number.parseFloat(tree.text || getNodeText(tree, document));
     case "String":
-      return tree.text!; // TODO actually handle strings
+      return tree.text || getNodeText(tree, document); // TODO actually handle strings
+    case "InfixExpr": {
+      const [lhsTree, opNode, rhsTree] = tree.children!.filter((n) => !!n.type);
+      console.log(tree.children!);
+      const lhs: any = evalExpression(lhsTree, document);
+      const rhs: any = evalExpression(rhsTree, document);
+      switch (opNode.type) {
+        case "OpAdd":
+          if (
+            typeof lhs != typeof rhs
+          ) {
+            throw new Error(
+              `TypeError: cannot add ${typeof lhs} and ${typeof rhs}`,
+            );
+          }
+
+          return lhs + rhs;
+        default:
+          throw new Error("unknown op " + opNode.type);
+      }
+    }
     default:
       throw new Error("cannot handle node type " + tree.type);
   }
